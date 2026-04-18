452528624c69b5d02167443c45eb9dd988427c28
NIKHIL KUMAR <chhaya.fut.dev@gmail.com>
2026-04-18 15:40:44 +0530

    modified:   index.html modified:   whatsapp-popup.js

--- whatsapp-popup.js
+++ whatsapp-popup.js
@@ -1,13 +1,13 @@
 function submitForm(e) {
   e.preventDefault();
-  const name = document.getElementById("name").value;
-  const phone = document.getElementById("phone").value;
-  const email = document.getElementById("email").value;
-  const message = document.getElementById("message").value;
+  const whatsapp_name = document.getElementById("whatsapp_name").value;
+  const whatsapp_phone = document.getElementById("whatsapp_phone").value;
+  const whatsapp_email = document.getElementById("whatsapp_email").value;
+  const whatsapp_message = document.getElementById("whatsapp_message").value;
 
-  const text = `Hello, I want to visit your CBNK - Store.%0AName: ${name}%0APhone: ${phone}%0AEmail: ${email}%0AMessage: ${message}`;
+  const text = `Hello, I want to visit your CBNK - Store.%0AName: ${whatsapp_name}%0APhone: ${whatsapp_phone}%0AEmail: ${whatsapp_email}%0AMessage: ${whatsapp_message}`;
 
-  const whatsappNumber = "919839507418"; // replace with your number
+  const whatsappNumber = "919839507418";
 
   window.open(`https://wa.me/${whatsappNumber}?text=${text}`, "_blank");
 }
